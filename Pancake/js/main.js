ccac825337a69bf22a0d45f9732077875c1f05b5
Njord25 <jordannjord@gmail.com>
2017-07-27 09:07:01 -0600

    Actualizado

--- Pancake/js/main.js
+++ Pancake/js/main.js
@@ -90,6 +90,45 @@ $(document).ready(function(){
 	    }
 	}
 
+
+//Text Animation
+var paragraph = document.getElementsByTagName('p')[0];
+
+function textEffect(animationName) {
+  var text = paragraph.innerHTML,
+		chars = text.length,
+		newText = '',
+		animation = animationName,
+		char,
+		i;
+
+	for (i = 0; i < chars; i += 1) {
+		newText += '<i>' + text.charAt(i) + '</i>';
+	}
+
+	paragraph.innerHTML = newText;
+
+	var wrappedChars = document.getElementsByTagName('i'),
+		wrappedCharsLen = wrappedChars.length,
+		j = 0;
+
+	function addEffect () {
+		setTimeout(function () {
+			wrappedChars[j].className = animation;
+			j += 1;
+			if (j < wrappedCharsLen) {
+				addEffect();
+			}
+		}, 300)
+	}
+
+	addEffect();
+};
+
+textEffect('fly-in-out');
+
+
+
 	function processing(hide, show){
 			$('#mc_submit i').removeClass(hide).addClass(show);
 	}
@@ -114,3 +153,5 @@ $(document).ready(function(){
 	});
 
 });
+
+
